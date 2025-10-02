bee6f460aad69e81ef0549a201c0fa4684238d50
alyasabrinaya-ui <alyasabrinaya@gmail.com>
2025-10-02 22:43:25 +0700

    250441100101-AlyaSabrina-EdiPutra-MODUL1

---
+++ modul-1/250441100101_AlyaSabrina/quiz1.py
@@ -0,0 +1,8 @@
+#quiz
+NIM = 101
+celcius = NIM 
+
+fahrenheit = (celcius * 1.8) + 32 
+kelvin = celcius + 273.15
+
+print("NIM saya adalah : " ,NIM ,"konversi fahrenheitnya : ", fahrenheit ,"konversi kelvinnya : ", kelvin )
