9dcc7587fb4ea1a38d5064a5a4243628abbb2115
25_137_MOH.RODIDULMA'ASYI <rodidulmaasyi@gmail.com>
2025-10-10 10:12:33 +0700

    Update tugas2_modul2.py

--- modul-2/tugas2_modul2.py
+++ modul-2/tugas2_modul2.py
@@ -21,22 +21,3 @@ harga_bayar = Harga_normal - (Harga_normal * diskon / 100)
 
 print("Diskon yang di dapatkan : ", diskon, "%")
 print("harga tiket yang harus di bayar: Rp", int(harga_bayar))
-
-# Program Menghitung Tarif Parkir Mall
-
-lama_parkir = int(input("Berapa lama kamu parkir(jam): "))
-
-vip = input("Apakah memmber VIP? (ya/tidak): ").lower()
-
-if vip == "ya":
-    total = 0
-else:
-    if lama_parkir <= 2:
-        total = 5000
-    else:
-        total = 5000 + (lama_parkir - 2) * 3000
-
-    if total >= 20000:
-        total = 20000
-
-print("Total biaya parkir: Rp", total)
