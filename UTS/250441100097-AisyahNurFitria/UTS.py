96e8d419f8e12f92d2f428d6bcba78bf16905d5c
fitriaaisyah <fitriaaisyah80@gmail.com>
2025-10-17 08:41:59 +0700

    250441100097_AisyahNurFitria

---
+++ UTS/250441100097-AisyahNurFitria/UTS.py
@@ -0,0 +1,12 @@
+#buat program rental motor
+
+motor_matic = 50.000
+motor_trail = 100.000
+motor_sport = 75.00000
+
+jenis_motor = input("masukkan jenis motor : ")
+
+asuransi = 15.000 ("berlaku kelipatan")
+lama_sewa = 3
+total = 0
+
