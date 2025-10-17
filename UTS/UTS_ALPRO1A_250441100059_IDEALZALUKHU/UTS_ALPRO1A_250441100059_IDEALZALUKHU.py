7511a7482523daf448a20a30f0e16a86a5709899
zalukhuideal <zalukhuideal@gmail.com>
2025-10-17 09:13:49 +0700

    UTS_ALPRO1D_250441100059_IDEALZALUKHU

---
+++ UTS/UTS_ALPRO1A_250441100059_IDEALZALUKHU/UTS_ALPRO1A_250441100059_IDEALZALUKHU.py
@@ -0,0 +1,26 @@
+motor_metic = 5000000
+motor_trail = 10000000
+motor_sport = 7500000
+asuransi = 1500000
+hari = 3
+kupon = "Accongg"    
+diskon = 0.10
+
+
+if hari > (asuransi):
+    print(diskon)
+    
+
+    
+
+
+
+
+    
+
+
+    
+
+
+    
+    
