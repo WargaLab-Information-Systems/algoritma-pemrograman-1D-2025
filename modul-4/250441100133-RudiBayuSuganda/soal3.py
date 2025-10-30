f39e1c5e79a333d41e81b796cf95843747110b92
Rudi Bayu Suganda <rudib6929@example.com>
2025-10-30 21:50:14 +0700

    250441100133-RudiBayuSuganda-FaisalDwiFirmansyah-MODUL 4

--- modul-4/250441100133-RudiBayuSuganda/soal3.py
+++ modul-4/250441100133-RudiBayuSuganda/soal3.py
@@ -11,3 +11,4 @@ for i in range(1,n+1):
     for k in range(i,0,-1):
          print(f"{k:{lebar}}", end="")
     print()
+    
--- modul-4/250441100133-RudiBayuSuganda/soal4.py
+++ modul-4/250441100133-RudiBayuSuganda/soal4.py
@@ -10,6 +10,4 @@ for i in range(1,n+1):
     
     for k in range(1,i+1):
          print(f"{k:{lebar}}", end="")
-    # # for k in range(i,0,-1):
-    #     print(f"{k:{lebar}}", end="")
     print()
