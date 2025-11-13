e84a09458e9307bffb78b635ca41939cc9db920c
intainnuraini040806 <intan.nuraini040806@gmail.com>
2025-11-13 11:25:28 +0700

    250441100125-IntanNuraini-FaisalDwiFirmansyah-MODUL 6

---
+++ modul-6/250441100125-IntanNurainiI/soal6.4.py
@@ -0,0 +1,9 @@
+for i in range (2):
+    mata_kuliah =['matematika','fisika','kimia','biologi']
+    print("jadi keseluruhan matkul adalah:",mata_kuliah)
+    mata_kuliah.append('pendidikan pancasi')
+    print("matkul baru",mata_kuliah)
+    mata_kuliah.append('bahasa inggris')
+    print("matkul baru",mata_kuliah)
+    print("jdi banyak mata kuliah adalah :",len(mata_kuliah))
+    print(mata_kuliah[2])
