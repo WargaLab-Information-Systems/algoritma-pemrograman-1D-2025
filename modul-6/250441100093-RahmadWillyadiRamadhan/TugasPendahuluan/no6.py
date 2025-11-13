f4b31f7cbe928231ec3d074a2f36f4f0712851fb
rahmadwillyadi <rahmadwillyadi123@gmail.com>
2025-11-13 22:31:51 +0700

    250441100093-RahmadWillyadiRamadhan-EdiPutra-modul6

---
+++ modul-6/250441100093-RahmadWillyadiRamadhan/TugasPendahuluan/no6.py
@@ -0,0 +1,11 @@
+# Membuat List awal berisi tiga item
+daftar_belanja = ["telur", "roti", "susu"]
+
+# Menambahkan item baru "keju" ke akhir daftar
+daftar_belanja.append("keju")
+
+# Menghapus item "roti" dari daftar
+daftar_belanja.remove("roti")
+
+# Menampilkan daftar belanja yang sudah diperbarui
+print("Daftar belanja terbaru:", daftar_belanja)
